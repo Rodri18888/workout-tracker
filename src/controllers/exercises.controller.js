dce24918b5d77696a6ec8d405d0c0dd65134d32e
Rodrigo <rodrigovelezdiaz@gmail.com>
2025-09-30 18:56:37 -0500

    metodos PUT y PATCH implementados (exercises)

--- src/controllers/exercises.controller.js
+++ src/controllers/exercises.controller.js
@@ -64,6 +64,60 @@ const createExercise = (req, res) => {
   res.status(201).json({ message: "Ejercicio creado", exercise: newExercise });
 };
 
+// Metodo PUT
+const updateExercise = (req, res) => {
+  const exerciseId = parseInt(req.params.id);
+  const { name, category, muscle_group, description, equipment, difficulty, duration, repetitions, sets } = req.body;
+
+  const index = ExercisesJSON.findIndex(e => e.id === exerciseId);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Ejercicio no encontrado" });
+  }
+
+  if (!name || !category || !muscle_group || !description || !equipment || !difficulty || !duration || !repetitions || !sets) {
+    return res.status(400).json({ error: "Faltan datos obligatorios" });
+  }
+
+  ExercisesJSON[index] = {
+    id: exerciseId,
+    name,
+    category,
+    muscle_group,
+    description,
+    equipment,
+    difficulty,
+    duration,
+    repetitions,
+    sets
+  };
+
+  res.status(200).json({ message: "Ejercicio actualizado (PUT)", exercise: ExercisesJSON[index] });
+};
+
+// Metodo PATCH
+const patchExercise = (req, res) => {
+  const exerciseId = parseInt(req.params.id);
+  const { name, category, muscle_group, description, equipment, difficulty, duration, repetitions, sets } = req.body;
+
+  const exercise = ExercisesJSON.find(e => e.id === exerciseId);
+
+  if (!exercise) {
+    return res.status(404).json({ error: "Ejercicio no encontrado" });
+  }
+
+  if (name) exercise.name = name;
+  if (category) exercise.category = category;
+  if (muscle_group) exercise.muscle_group = muscle_group;
+  if (description) exercise.description = description;
+  if (equipment) exercise.equipment = equipment;
+  if (difficulty) exercise.difficulty = difficulty;
+  if (duration) exercise.duration = duration;
+  if (repetitions) exercise.repetitions = repetitions;
+  if (sets) exercise.sets = sets;
+
+  res.status(200).json({ message: "Ejercicio actualizado parcialmente (PATCH)", exercise });
+};
 
 // Exportación
 module.exports = { getExerciseById, getExercises, createExercise, updateExercise, patchExercise, deleteExercise };
