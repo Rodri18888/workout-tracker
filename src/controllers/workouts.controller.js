9c0d51e1cf5ecdaafe2920a8c1ea784871ab30f8
rodrigo <rodrigovelezdiaz@gmail.com>
2025-09-30 20:08:43 -0500

    metodo PUT y PATCH (workouts)

--- src/controllers/workouts.controller.js
+++ src/controllers/workouts.controller.js
@@ -55,3 +55,50 @@ const createWorkout = (req, res) => {
   
     res.status(201).json({ message: "Workout creado", workout: newWorkout });
   };
+
+  // Metodo PUT
+const updateWorkout = (req, res) => {
+    const workoutId = parseInt(req.params.id);
+    const { user_id, name, exercises, duration, date } = req.body;
+  
+    const index = WorkoutsJSON.findIndex(w => w.id === workoutId);
+  
+    if (index === -1) {
+      return res.status(404).json({ error: "Workout no encontrado" });
+    }
+  
+    if (!user_id || !name || !exercises || !duration || !date) {
+      return res.status(400).json({ error: "Faltan datos obligatorios" });
+    }
+  
+    WorkoutsJSON[index] = {
+      id: workoutId,
+      user_id,
+      name,
+      exercises,
+      duration,
+      date
+    };
+  
+    res.status(200).json({ message: "Workout actualizado (PUT)", workout: WorkoutsJSON[index] });
+  };
+  
+  // Metodo PATCH
+  const patchWorkout = (req, res) => {
+    const workoutId = parseInt(req.params.id);
+    const { user_id, name, exercises, duration, date } = req.body;
+  
+    const workout = WorkoutsJSON.find(w => w.id === workoutId);
+  
+    if (!workout) {
+      return res.status(404).json({ error: "Workout no encontrado" });
+    }
+  
+    if (user_id) workout.user_id = user_id;
+    if (name) workout.name = name;
+    if (exercises) workout.exercises = exercises;
+    if (duration) workout.duration = duration;
+    if (date) workout.date = date;
+  
+    res.status(200).json({ message: "Workout actualizado parcialmente (PATCH)", workout });
+  };
