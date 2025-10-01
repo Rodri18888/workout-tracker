48430e4ccc8e884a94b4672aa86368b6b8382d59
rodrigo <rodrigovelezdiaz@gmail.com>
2025-09-30 20:19:49 -0500

    metodo GET (reports)

---
+++ src/controllers/reports.controller.js
@@ -0,0 +1,35 @@
+// Simulación base de datos
+const ReportsJSON = require("../data/reports.json");
+
+// Metodo GET ID
+const getReportById = (req, res) => {
+  const reportId = parseInt(req.params.id);
+  const report = ReportsJSON.find(r => r.id === reportId);
+
+  if (!report) {
+    return res.status(404).json({ error: "Reporte no encontrado" });
+  }
+
+  res.status(200).json(report);
+};
+
+// Metodo GET
+const getReports = (req, res) => {
+  const { user_id, workout_id, progress } = req.query;
+
+  let resultados = ReportsJSON;
+
+  if (user_id) {
+    resultados = resultados.filter(r => r.user_id === parseInt(user_id));
+  }
+
+  if (workout_id) {
+    resultados = resultados.filter(r => r.workout_id === parseInt(workout_id));
+  }
+
+  if (progress) {
+    resultados = resultados.filter(r => r.progress.toLowerCase() === progress.toLowerCase());
+  }
+
+  res.status(200).json(resultados);
+};
