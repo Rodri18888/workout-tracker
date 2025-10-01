993aef7359d28f7e9d4821b489287024686ddaeb
rodrigo <rodrigovelezdiaz@gmail.com>
2025-09-30 20:21:20 -0500

    metodos PUT y PATCH (reports)

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -55,3 +55,49 @@ const createReport = (req, res) => {
   
     res.status(201).json({ message: "Reporte creado", report: newReport });
   };
+
+  // Metodo PUT
+const updateReport = (req, res) => {
+    const reportId = parseInt(req.params.id);
+    const { user_id, workout_id, calories_burned, progress } = req.body;
+  
+    const index = ReportsJSON.findIndex(r => r.id === reportId);
+  
+    if (index === -1) {
+      return res.status(404).json({ error: "Reporte no encontrado" });
+    }
+  
+    if (!user_id || !workout_id || !calories_burned || !progress) {
+      return res.status(400).json({ error: "Faltan datos obligatorios" });
+    }
+  
+    ReportsJSON[index] = {
+      id: reportId,
+      user_id,
+      workout_id,
+      calories_burned,
+      progress,
+      created_at: ReportsJSON[index].created_at // mantener fecha original
+    };
+  
+    res.status(200).json({ message: "Reporte actualizado (PUT)", report: ReportsJSON[index] });
+  };
+  
+  // Metodo PATCH
+  const patchReport = (req, res) => {
+    const reportId = parseInt(req.params.id);
+    const { user_id, workout_id, calories_burned, progress } = req.body;
+  
+    const report = ReportsJSON.find(r => r.id === reportId);
+  
+    if (!report) {
+      return res.status(404).json({ error: "Reporte no encontrado" });
+    }
+  
+    if (user_id) report.user_id = user_id;
+    if (workout_id) report.workout_id = workout_id;
+    if (calories_burned) report.calories_burned = calories_burned;
+    if (progress) report.progress = progress;
+  
+    res.status(200).json({ message: "Reporte actualizado parcialmente (PATCH)", report });
+  };
